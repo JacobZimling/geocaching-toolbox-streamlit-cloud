da22b935978ce4b2cbb16d19f20483d743be0108
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-03-13 08:23:42 +0100

    Update gpscoordinates.py

--- modules/gpscoordinates.py
+++ modules/gpscoordinates.py
@@ -1,5 +1,6 @@
 import re
 import folium
+from modules import math as my_math
 
 
 degrees_minutes_regex: str = r"([NS])[ ]?(\d{1,3}).+?([0-5]\d)\.(\d{3}).+([EW])[ ]?(\d{1,3}).+?([0-5]\d)\.(\d{3}).*"
@@ -17,8 +18,8 @@ def standardize_coordinate(coordinate: str) -> str:
     return std
 
 
-def degrees_minutes_2_degrees(latlong: str) -> float:
-    items = re.search(r"([NSEW])(\d{1,3}) ([0-5]\d\.\d{3})", latlong)
+def degrees_minutes_2_degrees(lat_long: str) -> float:
+    items = re.search(r"([NSEW])(\d{1,3}) ([0-5]\d\.\d{3})", lat_long)
     if items.group(1) in ('N', 'E'):
         sign = 1
     else:
@@ -43,3 +44,32 @@ def show_on_map(coordinate: str, zoom: int = 13) -> folium.Map:
     m = folium.Map(location=location, zoom_start=zoom)
     folium.Marker(location, popup='Beregnet koordinat').add_to(m)
     return m
+
+
+def coordinate_sum_of_digits(std_coordinate: str) -> dict:
+    # sum_of_digits = {}
+    # parts = std_coordinate.split(' ')
+    parts = re.search(r"([NS])(\d{1,3} [0-5]\d\.\d{3}) ([EW])(\d{1,3} [0-5]\d\.\d{3})", std_coordinate)
+
+    # lat = str(int(parts[0]))+str(int(parts[1]))
+    # long = str(int(parts[2]))+str(int(parts[3]))
+
+    ns = parts.group(1)
+    lat = int(re.sub(r"\D", "", parts.group(2)))
+    ew = parts.group(3)
+    long = int(re.sub(r"\D", "", parts.group(4)))
+    lat_long = int(re.sub(r"\D", "", std_coordinate))
+
+    sum_of_digits = {
+        'Tværsum': {ns: my_math.sum_of_digits(lat), ew: my_math.sum_of_digits(long), 'Hele koordinatet': my_math.sum_of_digits(lat_long)},
+        'Reduceret tværsum': {ns: my_math.sum_of_digits_reduced(lat), ew: my_math.sum_of_digits_reduced(long), 'Hele koordinatet': my_math.sum_of_digits_reduced(lat_long)}
+    }
+    sum_of_digits = {
+        ns: {'Reduceret tværsum': my_math.sum_of_digits_reduced(lat), 'Tværsum': my_math.sum_of_digits(lat)},
+        ew: {'Reduceret tværsum': my_math.sum_of_digits_reduced(long), 'Tværsum': my_math.sum_of_digits(long)},
+        'Hele koordinatet': {'Reduceret tværsum': my_math.sum_of_digits_reduced(lat_long), 'Tværsum': my_math.sum_of_digits(lat_long)}
+        # 'Reduceret tværsum': {ns: my_math.sum_of_digits_reduced(lat), ew: my_math.sum_of_digits_reduced(long), 'Hele koordinatet': my_math.sum_of_digits_reduced(lat_long)},
+        # 'Tværsum': {ns: my_math.sum_of_digits(lat), ew: my_math.sum_of_digits(long), 'Hele koordinatet': my_math.sum_of_digits(lat_long)}
+    }
+
+    return sum_of_digits
