d77f8ab8ff761fcccba93451ad978a3ef62dfc1f
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-03-13 08:20:26 +0100

    Update Toolbox.py

--- Toolbox.py
+++ Toolbox.py
@@ -3,5 +3,8 @@ from modules import toolbox
 
 toolbox.toolbox_header("Velkommen til toolbox'en")
 
-# TODO: Welcome text describing the purpose of the toolbox
-# TODO: Link to github for reporting bugs
+# DONE: Welcome text describing the purpose of the toolbox
+toolbox.toolbox_description()
+
+# DONE: Link to GitHub for reporting bugs
+toolbox.toolbox_feedback()
