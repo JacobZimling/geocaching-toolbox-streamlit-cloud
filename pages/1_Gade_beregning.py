7a58df9f5e3bb8ffcd6cc504e701ca5b6fbe45b5
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-03-13 08:26:21 +0100

    Update 1_Gade_beregning.py

--- pages/1_Gade_beregning.py
+++ pages/1_Gade_beregning.py
@@ -3,24 +3,35 @@ from modules import gade
 from modules import toolbox
 from modules import gpscoordinates as gps
 import pandas as pd
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+import platform
 
 # DONE: Handle regex matching returning an error - try
 # TODO: GitHub bug reporting
 
 toolbox.toolbox_header("Beregn Gade")
 
+## Use default values for numbers_found and coordinate_formula when running on test server
+default_value = {}
+if platform.processor() == "":
+    default_value['numbers_found'] = ""
+    default_value['coordinate_formula'] = ""
+else:
+    default_value['numbers_found'] = "1644"
+    default_value['coordinate_formula'] = "N55 4e.jae E012 3a.fdd"
+    # default_value['coordinate_formula'] = "N 55.38.(1*(J+A))EI E 012 16.JEK"
+    # default_value['coordinate_formula'] = "D 55 38.JEI E 012 16.JEK"
+
 numbers_found = st.text_input(
     label="Indtast de fundne tal. Bogstaver konverteres til deres nummer i alfabetet.",
     placeholder="Indtast de fundne tal",
-    # value="1535114"
+    value=default_value['numbers_found']
 )
 
 coordinate_formula = st.text_input(
     label="Indtast formlen til at udregne koordinatet",
     placeholder="Nnn nn.nnn Ennn nn.nnn",
-    # value="N 55.38.(1*(J+A))EI E 012 16.JEK"
-    # value="D 55 38.JEI E 012 16.JEK"
+    value=default_value['coordinate_formula']
 )
 
 st.button("Beregn Gade")
@@ -39,19 +50,16 @@ if numbers_found and coordinate_formula:
     # Display all numbers
     # DONE: Remove index when printing dataframe
     st.write('Alle tal til formlen')
-    df = pd.DataFrame(data=[all_numbers.values()], columns=all_numbers.keys())
+    df = pd.DataFrame(data=[all_numbers.values()], columns=list(all_numbers.keys()))
     st.dataframe(df, hide_index=True)
 
     # TODO: Check if number of numbers match formula
     # Check if # numbers found matches pattern
 
-    # Substitute numbers in coordinate formula
-    result_coordinate_parts = gade.substitute_numbers(coordinate_formula_parts, all_numbers)
+    # Calculate coordinate using the numbers found
+    result_coordinate = gade.calculate_coordinate(coordinate_formula_parts, all_numbers)
 
-    # Resolve formula elements containing calculations
-    result_coordinate = gade.resolve_calculations(coordinate_formula_parts)
-
-    # Done: Check if calculated coordinate is a valid coordinate (Ndd dd.ddd Eddd dd.ddd)
+    # Done: Check if calculated coordinate is a valid coordinate (Nnn nn.nnn Ennn nn.nnn)
     result_is_coordinate = gps.is_coordinate(result_coordinate)
     if not result_is_coordinate:
         st.write('Beregningen giver ikke et koordinat')
@@ -66,7 +74,11 @@ if numbers_found and coordinate_formula:
     st.code(result_coordinate, language=None)
 
     # TODO: Calculate checksum and reduced checksum
+    st.write('Tværsummer')
+    st.table(gps.coordinate_sum_of_digits(result_coordinate))
 
     # DONE: Show on map
     if gps.is_coordinate(result_coordinate):
-        st_data = st_folium(gps.show_on_map(result_coordinate, zoom=14))
+        folium_static(gps.show_on_map(result_coordinate, zoom=14))
+
+toolbox.toolbox_feedback()
