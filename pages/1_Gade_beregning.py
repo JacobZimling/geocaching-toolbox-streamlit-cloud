26f50e147dec7c0557216167fb9a9ddbb20e0662
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2025-08-06 15:46:26 +0200

    Update 1_Gade_beregning.py

--- pages/1_Gade_beregning.py
+++ pages/1_Gade_beregning.py
@@ -5,6 +5,7 @@ from modules import gpscoordinates as gps
 import pandas as pd
 from streamlit_folium import folium_static
 import platform
+from st_copy import copy_button
 
 # DONE: Handle regex matching returning an error - try
 # TODO: GitHub bug reporting
@@ -72,7 +73,13 @@ if numbers_found and coordinate_formula:
     # Display result coordinate
     # TODO: Change font to default app font
     st.write('Beregnet koordinat')
-    st.code(result_coordinate, language=None)
+    st.write(result_coordinate, language=None)
+    msg = 'some message'
+    copy_button(
+        result_coordinate,
+        icon='st',
+        key='result_coordinate,
+    )
     if not result_is_coordinate:
         st.stop()
 
@@ -86,6 +93,7 @@ if numbers_found and coordinate_formula:
             folium_static(gps.show_on_map(result_coordinate, zoom=14))
 
 toolbox.toolbox_feedback()
+
 
 
 
